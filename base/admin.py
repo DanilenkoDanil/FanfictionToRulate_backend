ca8b0a7c3455c2bbd88681bb5b2390b3d47a31e0
ilona <ilonakvy@gmail.com>
2023-02-20 02:47:54 +0200

    base update

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Book, Chapter, User
 
-# Register your models here.
+
+@admin.register(Book)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre', 'fandom', 'link', 'status')
+    search_fields = ('name', 'status')
+
+
+@admin.register(Chapter)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('book', 'number', 'name', 'status', 'data')
+    search_fields = ('book', 'name')
+
+
+@admin.register(User)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'iqos', 'status')
+
--- base/models.py
+++ base/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=150)
+    genre = models.CharField(max_length=50)
+    fandom = models.CharField(max_length=50)
+    link = models.CharField(max_length=150)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Chapter(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    number = models.PositiveIntegerField()
+    name = models.CharField(max_length=150)
+    text = models.TextField()
+    status = models.BooleanField(default=False)
+    data = models.DateTimeField(auto_now=True)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=150)
+    telegram_id = models.CharField(max_length=150)
+    iqos = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
+
+
+
